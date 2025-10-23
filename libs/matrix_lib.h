157ee1e06b7bcf9c5fb35c7be082535afd3909dd
Marco-Garro <mgarro@elemento.cloud>
2025-10-23 14:16:28 +0200

    really better way to set to zero a column

--- libs/matrix_lib.h
+++ libs/matrix_lib.h
@@ -10,7 +10,6 @@
 float** readMatrix(char* filename, int rows, int columns);
 void displayMatrix(float** matrix, int rows, int columns);
 float* getColumn(float** matrix, int rows, int column);
-float findLambda(float pivot, float element);
 void killColumn(float** matrix, int rows, int columns);
 void swapRows(float** matrix, int a, int b);
 
@@ -61,43 +60,15 @@ inline float* getColumn(float** matrix, int rows, int column) {
     return cols;
 }
 
-
-inline float findLambda(float pivot, float element) {   // TODO find a better way to manage float values
-    float lambda = 1;
-    int pivotSign = pivot > 0?1:-1;
-    int elementSign = element > 0?1:-1;
-
-    int convertedPivot = (int) pivot * pivotSign;
-    int convertedElement = (int) element * elementSign;
-
-    int elementMultiplier = 1;
-    int pivotMultiplier = 1;
-    if (pivotSign * pivot > elementSign * element) {
-        while (convertedElement * elementMultiplier % convertedPivot)
-            elementMultiplier++;
-
-        pivotMultiplier = convertedElement * elementMultiplier / convertedPivot;
-        lambda = (float) pivotMultiplier / (float) elementMultiplier;
-    }
-
-    else {
-        while (convertedPivot * pivotMultiplier % convertedElement)
-            pivotMultiplier++;
-        elementMultiplier = convertedPivot * pivotMultiplier / convertedElement;
-        lambda = (float) pivotMultiplier / (float) elementMultiplier;
-    }
-
-    printf("lambda = %.2f\n", lambda * (elementSign * pivotSign));
-    return lambda * (elementSign * pivotSign);
-}
-
 inline void killColumn(float** matrix, int rows, int columns) {
     float pivot = matrix[0][0];
     for (int i = 1; i < rows; i++) {
         if (matrix[i][0] != 0) {
-            float lambda = findLambda(pivot,matrix[i][0]);
-            for (int j = 0; j < columns; j++)   // iterate on the column of the pivot
-                matrix[i][j] -= lambda * matrix[0][j];
+            float toKill = matrix[i][0];
+            for (int j = 0; j < columns; j++) { // iterate on the column of the pivot
+                matrix[i][j] *= pivot;
+                matrix[i][j] -= matrix[0][j] * toKill;
+            }
         }
     }
 }
